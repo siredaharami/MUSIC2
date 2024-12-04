f1d897aae369c2afc8881bfd3f31c923f09f1ab1
Teamonix <unioxo@outlook.com>
2024-12-04 18:53:31 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -1,6 +1,6 @@
 import os
 import importlib
-from testing import app, bot
+from testing import bot, app
 
 def load_plugins():
     plugin_dir = "testing/modules"
@@ -17,87 +17,6 @@ if __name__ == "__main__":
     load_plugins()
     print("All plugins imported successfully.")
     print("Bot is running...")
-    app.run()
-    bot.run() 
-
-
-
-async def main():
-    LOGGER.info("🐬 Updating Directories ...")
-    if "cache" not in os.listdir():
-        os.mkdir("cache")
-    if "cookies.txt" not in os.listdir():
-        LOGGER.info("⚠️ 'cookies.txt' - Not Found❗")
-        sys.exit()
-    if "downloads" not in os.listdir():
-        os.mkdir("downloads")
-    for file in os.listdir():
-        if file.endswith(".session"):
-            os.remove(file)
-    for file in os.listdir():
-        if file.endswith(".session-journal"):
-            os.remove(file)
-    LOGGER.info("✅ All Directories Updated.")
-    await asyncio.sleep(1)
-    LOGGER.info("🌐 Checking Required Variables ...")
-    if API_ID == 0:
-        LOGGER.info("❌ 'API_ID' - Not Found ‼️")
-        sys.exit()
-    if not API_HASH:
-        LOGGER.info("❌ 'API_HASH' - Not Found ‼️")
-        sys.exit()
-    if not BOT_TOKEN:
-        LOGGER.info("❌ 'BOT_TOKEN' - Not Found ‼️")
-        sys.exit()
-    if not STRING_SESSION:
-        LOGGER.info("❌ 'STRING_SESSION' - Not Found ‼️")
-        sys.exit()
-
-    if not MONGO_DB_URL:
-        LOGGER.info("'MONGO_DB_URL' - Not Found !!")
-        sys.exit()
-    try:
-        await mongo_async_cli.admin.command('ping')
-    except Exception:
-        LOGGER.info("❌ 'MONGO_DB_URL' - Not Valid !!")
-        sys.exit()
-    LOGGER.info("✅ Required Variables Are Collected.")
-    await asyncio.sleep(1)
-    LOGGER.info("🌀 Starting All Clients ...")
-    try:
-        await bot.start()
-    except Exception as e:
-        LOGGER.info(f"🚫 Bot Error: {e}")
-        sys.exit()
-    if LOG_GROUP_ID != 0:
-        try:
-            await bot.send_message(LOG_GROUP_ID, "**🤖 Bot Started.**")
-        except Exception:
-            pass
-    LOGGER.info("✅ Bot Started.")
-    try:
-        await app.start()
-    except Exception as e:
-        LOGGER.info(f"🚫 Assistant Error: {e}")
-        sys.exit()
-    try:
-        await app.join_chat("AdityaServer")
-        await app.join_chat("AdityaDiscus")
-    except Exception:
-        pass
-    if LOG_GROUP_ID != 0:
-        try:
-            await app.send_message(LOG_GROUP_ID, "**🦋 Assistant Started.**")
-        except Exception:
-            pass
-    LOGGER.info("✅ Assistant Started.")
-    try:
-        await call.start()
-    except Exception as e:
-        LOGGER.info(f"🚫 PyTgCalls Error: {e}")
-        sys.exit()
-    LOGGER.info("✅ PyTgCalls Started.")
-    await asyncio.sleep(1)
-    LOGGER.info("✅ Sucessfully Hosted Your Bot !!")
-    LOGGER.info("✅ Now Do Visit: @AdityaServer !!")
+    await bot.start() 
+    await app.start() 
     await idle()
