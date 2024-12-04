f75914bf8eaa2024a0bf85ec79b2576a66badea5
Teamonix <unioxo@outlook.com>
2024-12-04 16:28:49 +0530

    Update __init__.py

--- testing/__init__.py
+++ testing/__init__.py
@@ -2,12 +2,11 @@
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-from testing.handlers import register_handlers
 
 class MyTelegramBot(Client):
     def __init__(self):
         super().__init__("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-        register_handlers(self)
+        
 
     def run(self):
         self.start()
