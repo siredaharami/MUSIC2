849dcdec3ec13b6634e17336748be67d58a0e159
Teamonix <unioxo@outlook.com>
2024-12-04 15:06:23 +0530

    Update __init__.py

--- testing/__init__.py
+++ testing/__init__.py
@@ -5,9 +5,20 @@ api_id = 12345  # Replace with your actual API ID
 api_hash = "0123456789abcdef0123456789abcdef"  # Replace with your actual API Hash
 bot_token = "" # Replace with your Bot-Token
 
+
 app = Client(
-         "testing",
-          api_id=config.API_ID, 
-          api_hash=config.API_HASH, 
-          bot_token=config.BOT_TOKEN
+    name="App",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    session_string=str(config.STRING_SESSION),
 )
+
+bot = Client(
+    name="Bot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+)
+
+call = PyTgCalls(app)
+call_config = GroupCallConfig(auto_start=False)
