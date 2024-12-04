7ede733d783e729f7d84d3b932439c7a5fba8dab
Teamonix <unioxo@outlook.com>
2024-12-04 17:08:58 +0530

    Update __init__.py

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,3 +1,4 @@
+import os
 from pyrogram import Client
 from config import BOT_TOKEN, API_ID, API_HASH
 
@@ -6,5 +7,6 @@ app = Client(
     "testing",
     bot_token=BOT_TOKEN,
     api_id=API_ID,
-    api_hash=API_HASH
+    api_hash=API_HASH, 
+    plugins=dict(root="testing.modules")
 )
