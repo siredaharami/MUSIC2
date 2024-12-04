6adaf64e385523cb8378b3817dc7e466ced8677e
Teamonix <unioxo@outlook.com>
2024-12-04 14:51:47 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -1,1 +1,11 @@
+import asyncio
+from testing import app  # Import the app from init.py
+from pyrogram import filters
 
+async def main():
+    async with app:
+        print("Bot is running...")
+        await app.idle()  # Keep the bot running
+
+if __name__ == "__main__":
+    asyncio.run(main())
