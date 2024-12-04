ac372ea293faa3b45b56394c51746b2f3c2f08f9
Teamonix <unioxo@outlook.com>
2024-12-04 17:45:38 +0530

    Update n.py

--- testing/modules/n.py
+++ testing/modules/n.py
@@ -1,7 +1,7 @@
 import aiohttp, aiofiles, asyncio, base64, logging
 import os, platform, random, re, socket
 import sys, time, textwrap
-
+import config
 from os import getenv
 from io import BytesIO
 from time import strftime
@@ -74,18 +74,7 @@ logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 LOGGER = logging.getLogger("SYSTEM")
 
 
-# config variables
-if os.path.exists("Config.env"):
-    load_dotenv("Config.env")
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", 0))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
-START_IMAGE_URL = getenv("START_IMAGE_URL", None)
 
 
 # Memory Database
