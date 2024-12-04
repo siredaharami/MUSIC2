3659a810694ce68d014e4ec8603932a3e0bb86b9
Teamonix <unioxo@outlook.com>
2024-12-04 17:05:13 +0530

    Create __init__.py

---
+++ testing/__init__.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from config import BOT_TOKEN, API_ID, API_HASH
+
+# Create the Pyrogram Client
+app = Client(
+    "testing",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
