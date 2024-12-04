bc234106a1aae4e85ae765074600ec90220e1435
Teamonix <unioxo@outlook.com>
2024-12-04 15:09:23 +0530

    Update __init__.py

--- testing/__init__.py
+++ testing/__init__.py
@@ -4,11 +4,6 @@ from pytgcalls import PyTgCalls, filters
 from pytgcalls.types import GroupCallConfig
 
 
-api_id = 12345  # Replace with your actual API ID
-api_hash = "0123456789abcdef0123456789abcdef"  # Replace with your actual API Hash
-bot_token = "" # Replace with your Bot-Token
-
-
 app = Client(
     name="App",
     api_id=config.API_ID,
