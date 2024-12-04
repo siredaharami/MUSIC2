5a4595e9ca4c24811fa44f475de661af7a3a5569
Teamonix <unioxo@outlook.com>
2024-12-04 17:17:25 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -4,18 +4,14 @@ from testing import app
 
 def load_plugins():
     plugin_dir = "testing/modules"
-    plugin_package = "testing.modules"
+    
 
     for file_name in os.listdir(plugin_dir):
         if file_name.endswith(".py") and not file_name.startswith("__"):
             module_name = file_name[:-3]  # Remove the .py extension
-            importlib.import_module(f"{plugin_package}.{module_name}")
+            importlib.import_module(f"{plugin_dir}.{module_name}")
             print(f"Successfully imported: {module_name}")
 
-@app.on_message()
-async def handle_message(client, message):
-    if message.text:
-        await message.reply_text(f"You said: {message.text}")
 
 if __name__ == "__main__":
     print("Loading plugins...")
