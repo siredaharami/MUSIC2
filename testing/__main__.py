17f0db0774f8d265d2a2c7402c23dc0d84383f4b
Teamonix <unioxo@outlook.com>
2024-12-04 15:57:38 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -1,23 +1,14 @@
 import asyncio
-import importlib
-import sys 
 from pyrogram import idle
-from testing.modules import ALL_MODULES
-from testing import app, bot # Import the app from init.py
+from testing import app # Import the app from init.py
 from pyrogram import filters
 
 async def main():
     async with app:
-        await app.start()
-        for all_module in ALL_MODULES:
-            importlib.import_module("testing.modules" + all_module)
-        LOGGER("testing.modules").info(
-        "Successfully Imported Modules"
-        )
+    
         print("Bot is running...")
-        await bot.start()
+
         await app.idle()  # Keep the bot running
         
 if __name__ == "__main__":
-    loop.run_until_complete(init())
-    LOGGER("testing").info("Stopping Yukki Music Bot! GoodBye")
+    app.run()
