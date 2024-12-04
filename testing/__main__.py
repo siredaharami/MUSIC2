5e437811cd27b90a38120fad92ac8c40ce82b30e
Teamonix <unioxo@outlook.com>
2024-12-04 15:22:08 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -1,11 +1,21 @@
 import asyncio
+import importlib
+import sys 
+from pyrogram import idle
+from testing.modules import ALL_MODULES
 from testing import app  # Import the app from init.py
 from pyrogram import filters
 
 async def main():
     async with app:
+        await app.start()
+        for all_module in ALL_MODULES:
+        importlib.import_module("testing.modules" + all_module)
+        LOGGER("testing.modules").info(
+        "Successfully Imported Modules"
+        )
         print("Bot is running...")
         await app.idle()  # Keep the bot running
-
+        
 if __name__ == "__main__":
     asyncio.run(main())
