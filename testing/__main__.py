a36aa4182b451e431b27258aa29135185fdcf187
Teamonix <unioxo@outlook.com>
2024-12-04 19:14:31 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -2,7 +2,7 @@ import os
 import importlib
 from testing import bot
 
-def loaded_plugins():
+def load_plugins():
     plugin_dir = "testing/modules"
     plugin_package = "testing.modules"
 
@@ -11,13 +11,10 @@ def loaded_plugins():
             module_name = file_name[:-3]  # Remove the .py extension
             importlib.import_module(f"{plugin_package}.{module_name}")
             print(f"Successfully imported: {module_name}")
-            
-
 
 if __name__ == "__main__":
     print("Loading plugins...")
-    loaded_plugins()
-    bot.start()  
+    load_plugins()
     print("All plugins imported successfully.")
     print("Bot is running...")
-     
+    bot.run()
