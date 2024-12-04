d81eca69540e6c61b7a9ce0bbb4004452ec1d041
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-04 16:48:56 +0530

    Update __main__.py

--- testing/__main__.py
+++ testing/__main__.py
@@ -8,7 +8,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")  # Point to the plugins folder
+    plugins=dict(root="testing.plugins")  # Point to the plugins folder
 )
 
 # Start the bot
